0a4a702ea468c2c80f309a0663fed3009f7da1a3
Yi Chen <chenyiapp@yahoo.com>
2024-01-02 21:06:36 -0800

    auth

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    //   <Button variant="destructive" size='lg'>Button</Button>
+    <p>dashboard (protected) </p>
+  );
+}
