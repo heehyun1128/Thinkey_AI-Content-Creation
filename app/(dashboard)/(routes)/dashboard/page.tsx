e28362e91644b6fd31a0925822c8a08e68fb4685
Yi Chen <chenyiapp@yahoo.com>
2024-01-26 15:38:16 -0800

    sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
     //   <Button variant="destructive" size='lg'>Button</Button>
-    <p>dashboard (protected) </p>
+    <div>
+      <p>dashboard (protected) </p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   );
 }
