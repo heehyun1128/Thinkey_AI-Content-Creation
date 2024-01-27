732e4cca5e7d51744207e2611f9fd5bd356751a6
Yi Chen <chenyiapp@yahoo.com>
2024-01-26 17:21:17 -0800

    sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+
 
 export default function DashboardPage() {
   return (
     //   <Button variant="destructive" size='lg'>Button</Button>
     <div>
       <p>dashboard (protected) </p>
-      <UserButton afterSignOutUrl="/"/>
+      
     </div>
   );
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,17 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       {/*style side bar using tailwind css */}
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-600">
-        <div>Sidebar</div>
+        <Sidebar />
       </div>
       <main className="md:pl-72">
-        Content
+      <Navbar/>
+        {children}
+        
       </main>
     </div>
   );
