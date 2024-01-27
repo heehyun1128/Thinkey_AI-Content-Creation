111c0572eabae0f8ae278b50c8474131ec650c02
Yi Chen <chenyiapp@yahoo.com>
2024-01-26 23:22:34 -0800

    landing page tool selections

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,19 +1,44 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
-import { MessagesSquare } from "lucide-react";
+import { Palette,AudioWaveform,PlaySquare, MessagesSquare, PlusIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const tools = [
   {
     label: "Chat",
     icon: MessagesSquare,
     color: "text-gray-500",
-    bgColor: "bg-sky-500/10",
+    bgColor: "bg-gray-500/10",
     href: "/chat",
+  },
+  {
+    label: "AI Image",
+    icon: Palette,
+    color: "text-green-500",
+    bgColor: "bg-green-500/10",
+    href: "/image",
+  },
+  {
+    label: "AI Audio",
+    icon: AudioWaveform,
+    color: "text-sky-500",
+    bgColor: "bg-sky-500/10",
+    href: "/audio",
+  },
+  {
+    label: "AI Video",
+    icon: PlaySquare,
+    color: "text-purple-500",
+    bgColor: "bg-purple-500/10",
+    href: "/video",
   },
 ];
 
 export default function DashboardPage() {
+
+  const router = useRouter()
   return (
     //   <Button variant="destructive" size='lg'>Button</Button>
     <div>
@@ -30,8 +55,20 @@ export default function DashboardPage() {
         {tools.map((tool) => (
           <Card 
           key={tool.href}
+          onClick={()=>router.push(tool.href)}
           className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
-          ></Card>
+          >
+
+            <div className="items-center flex gap-x-3">
+              <div className={cn("p-2 w-fit rounded-md",tool.bgColor)}>
+                <tool.icon className={cn("w-10 h-10",tool.color)}/>
+              </div>
+              <div>
+                {tool.label}
+              </div>
+            </div>
+            <PlusIcon className="w-6 h-6 text-gray-400"/>
+          </Card>
         ))}
       </div>
     </div>
