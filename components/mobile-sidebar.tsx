c41a9c0c95056e070bbdbe6139f7bfddd352130c
Yi Chen <chenyiapp@yahoo.com>
2024-01-26 18:51:28 -0800

    remove hydrating error

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,25 +1,37 @@
 "use client";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
-import { Sheet, SheetTrigger,SheetContent } from "@/components/ui/sheet";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 
-import Sidebar from '@/components/sidebar'
+import Sidebar from "@/components/sidebar";
+import { useState, useEffect } from "react";
+
 
 
 const MobileSidebar = () => {
-  return (
-    <Sheet>
-      <SheetTrigger>
-        {/* click button to trigger sheet open or close */}
-        <Button size="icon" className="md:hidden">
-          <Menu />
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left" className="p-0">
-        <Sidebar />
-      </SheetContent>
-    </Sheet>
-  );
+  // remove hydrating error
+    const [isMounted,setIsMounted] = useState(false)
+
+    useEffect(()=>{
+        setIsMounted(true)
+    },[])
+
+    if(!isMounted){
+        return null
+    }
+    return (
+        <Sheet>
+        <SheetTrigger>
+            {/* click button to trigger sheet open or close */}
+            <Button size="icon" className="md:hidden">
+            <Menu />
+            </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="p-0">
+            <Sidebar />
+        </SheetContent>
+        </Sheet>
+    );
 };
 
 export default MobileSidebar;
