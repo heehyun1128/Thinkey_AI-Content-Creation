0fb3bb24635327059ad4ec50cc87c9542ad8fac8
Yi Chen <chenyiapp@yahoo.com>
2024-02-01 15:49:49 -0800

    chat constants

--- app/(dashboard)/(routes)/chat/page.tsx
+++ app/(dashboard)/(routes)/chat/page.tsx
@@ -1,7 +1,12 @@
+"use client"
 import { Heading } from "@/components/heading"
 import { MessagesSquare } from "lucide-react"
+import {useForm} from 'react-hook-form'
+
+import * as z from "zod"
 
 const ChatPage=()=>{
+    const form=useForm({defaultValues:{prompt:""}})
     return (
         <div>
             <Heading 
@@ -11,7 +16,11 @@ const ChatPage=()=>{
             iconColor="text-orange-500"
             bgColor="bg-orange-500/10"
             />
+            <div className="px-4 lg:px-8">
+
+            </div>
         </div>
+
     )
 }
 
