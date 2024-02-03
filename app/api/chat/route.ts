d1fd2b6219d70eb82121d4cdda9a25ce7a5e8ee2
Yi Chen <chenyiapp@yahoo.com>
2024-02-03 15:41:14 -0800

    chat api

---
+++ app/api/chat/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!openai.apiKey) {
+      return new NextResponse("OpenAI API key is missing.", { status: 500 });
+    }
+    if (!messages) {
+      return new NextResponse("Messages are required.", { status: 400 });
+    }
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages
+    });
+    return NextResponse.json(response.choices[0].message)
+  } catch (e) {
+    console.log("error:", e);
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
