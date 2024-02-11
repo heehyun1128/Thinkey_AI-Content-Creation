4bed76fe09faf89326e247653ca400652d5ddf51
Yi Chen <chenyiapp@yahoo.com>
2024-02-10 22:45:07 -0800

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head><link rel="icon" href="/tlogo.jpg" /></head>
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
