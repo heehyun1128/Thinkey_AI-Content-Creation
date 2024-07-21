ff9dd5ad4a98228196837577928cbaf7724d77d6
Yi Chen <chenyiapp@yahoo.com>
2024-07-21 16:32:21 -0700

    UI changes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,10 +5,10 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       {/*style side bar using tailwind css */}
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-600">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80]">
         <Sidebar />
       </div>
-      <main className="md:pl-72">
+      <main className="md:max-w-[calc(100%-72px)]">
       <Navbar/>
         {children}
         
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { LayoutPanelLeft,MessagesSquare,Palette,AudioWaveform,PlaySquare,Settings } from "lucide-react";
+import {
+  LayoutPanelLeft,
+  MessagesSquare,
+  Palette,
+  AudioWaveform,
+  PlaySquare,
+  Menu,
+  MoveLeft
+} from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 import Image from "next/image";
-
-import {cn} from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 const routes = [
   {
@@ -39,42 +46,54 @@ const routes = [
     href: "/video",
     color: "text-purple-500",
   },
-  // {
-  //   label: "Settings",
-  //   icon: Settings,
-  //   href: "/settings",
-  //   color: "text-gray-500",
-  // },
 ];
 
-
 const Sidebar = () => {
-    const pathname=usePathname()
+  const pathname = usePathname();
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-[#f6e399] text-gray">
-      <div className="px-6 py-4 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <h1 className="text-xl">
-            <div className="relative w-36 h-8 p-5 ml-8">
-            <Image fill src="/logo.jpg" alt="logo" />
-            </div>
-          </h1>
-        </Link>
-        <div className="space-y-1">
-          {routes.map((route) => (
-            <Link
-              href={route.href}
-              key={route.href}
-              className={cn("text-base group flex w-full p-5 justify-start cursor-pointer hover:text-white hover:bg-gray-500/30",
-              pathname===route.href? "text-white bg-gray-500/30": "text-gray"
-              )}
-            >
-              <div className="flex">
-                <route.icon className={route.color} />
-                <p className="px-3">{route.label}</p>
+    <div className="flex flex-col h-full">
+      <button
+        className={`fixed left-6 top-10 z-50 ${
+          showSidebar ? "text-4xl text-black" : "text-sm text-black"
+        }`}
+        onClick={() => setShowSidebar(!showSidebar)}
+      >
+        {showSidebar ? <MoveLeft />: <Menu />}
+      </button>
+      <div
+        className={`fixed inset-y-0 left-0 transform bg-gray-600 transition-transform duration-300 ease-in-out ${
+          showSidebar ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="space-y-4 py-4 flex flex-col h-full bg-[#f6e399] text-gray">
+          <div className="px-6 py-4 flex-1">
+            <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+              <div className="relative w-36 h-8 p-5 ml-8">
+                <Image fill src="/logo.jpg" alt="logo" />
               </div>
             </Link>
-          ))}
+            <div className="space-y-1">
+              {routes.map((route) => (
+                <Link
+                  href={route.href}
+                  key={route.href}
+                  className={cn(
+                    "text-base group flex w-full p-5 justify-start cursor-pointer hover:text-white hover:bg-gray-500/30",
+                    pathname === route.href
+                      ? "text-white bg-gray-500/30"
+                      : "text-gray"
+                  )}
+                >
+                  <div className="flex">
+                    <route.icon className={route.color} />
+                    <p className="px-3">{route.label}</p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -82,3 +101,4 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
